c0a380a60f0890affd1485eacaf17339751be10f
zsiskos <43017972+zsiskos@users.noreply.github.com>
2020-06-15 08:20:08 -0400

    Updates

---
+++ precache-manifest.64dd4c0f291e3dbccabf20c292e332f7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "69db3d0946d43a321d140170ca26dca6",
+    "url": "/mintbeant-GA-hackathon-wed/index.html"
+  },
+  {
+    "revision": "a0d4095f20a262fe42eb",
+    "url": "/mintbeant-GA-hackathon-wed/static/css/main.6afa6d8b.chunk.css"
+  },
+  {
+    "revision": "50928710ba211e271b48",
+    "url": "/mintbeant-GA-hackathon-wed/static/js/2.6ff8464d.chunk.js"
+  },
+  {
+    "revision": "066a3b5f5d30e9efcd56434ebe4db621",
+    "url": "/mintbeant-GA-hackathon-wed/static/js/2.6ff8464d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a0d4095f20a262fe42eb",
+    "url": "/mintbeant-GA-hackathon-wed/static/js/main.9ff46220.chunk.js"
+  },
+  {
+    "revision": "9fb7048c372651bfbea0",
+    "url": "/mintbeant-GA-hackathon-wed/static/js/runtime-main.34fdeb5d.js"
+  }
+]);
